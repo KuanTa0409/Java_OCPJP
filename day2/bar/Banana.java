a87f16150cd99107beb55f1f029d8e8c9b30f622
KuanTa,Lin <114749315+KuanTa0409@users.noreply.github.com>
2022-10-21 20:15:45 +0800

    Delete day2 directory

--- day2/bar/Banana.java
+++
@@ -1,9 +0,0 @@
-package com.ocp.day2.bar;
-
-public class Banana {
-    public static void baba(){
-        Apple.a1();
-        Apple.a2(); 
-//不能呼叫Apple的a3 因為a3是private
-    }
-}
--- day2/foo/Cherry.java
+++
@@ -1,9 +0,0 @@
-package com.ocp.day2.foo;
-
-import com.ocp.day2.bar.Apple; // 因為不同資料夾
-
-public class Cherry {
-    public static void cici(){
-        Apple.a1();
-    }
-}
