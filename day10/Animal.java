788345ddb33a9c9f991cdc240e9858c12510c944
KuanTa,Lin <114749315+KuanTa0409@users.noreply.github.com>
2022-11-18 21:37:15 +0800

    Delete day10 directory

--- day10/Animal.java
+++
@@ -1,7 +0,0 @@
-package com.ocp.day10;
-
-public interface Animal {
-    String COPY_RIGHT = "ABC.com"; //public final String COPY_RIGHT = "ABC.com";
-    void eat(); //相當於public abstract void eat();
-    void play(); //public abstract void play();
-}
--- day10/Cat.java
+++
@@ -1,9 +0,0 @@
-package com.ocp.day10;
-
-public class Cat extends AbstractAnimal {  
-    @Override
-    public void play() {
-        System.out.println("玩彩帶");
-    }
-    
-}
