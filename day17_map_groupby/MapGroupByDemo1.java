a345443bb7777c530c367f283e0e994a5ae38296
KuanTa,Lin <114749315+KuanTa0409@users.noreply.github.com>
2022-12-12 21:29:14 +0800

    Add files via upload

---
+++ day17_map_groupby/MapGroupByDemo1.java
@@ -0,0 +1,42 @@
+package com.ocp.day17_map_groupby;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class MapGroupByDemo1 {
+    public static void main(String[] args) {
+        List<String> fruits = 
+                Arrays.asList("apple", "apple", "banana", "orange", "orange", "apple", "papaya", "papaya", "watermelon");
+        
+        // 每一種水果有幾個
+        // {papaya=2, orange=2, banana=1, apple=3, watermelon=1}
+        Map<String, Long> result = fruits.stream()
+                                         .collect(
+                                             Collectors.groupingBy(
+                                                 //name -> name, Collectors.counting()
+                                                 Function.identity(), Collectors.counting()
+                                             )
+                                         );
+        
+        System.out.println(result);
+      
+        System.out.println(result);
+        // 在 result 中根據 value 大小排序
+        // 原本: {papaya=2, orange=2, banana=1, apple=3, watermelon=1}
+        // 變為: {banana=1, watermelon=1, papaya=2, orange=2, apple=3}
+        result.entrySet().stream()
+                //.sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .sorted(Map.Entry.<String, Long>comparingByValue())
+                .forEachOrdered(e -> System.out.println(e));
+        
+        Map<String, Long> resultSortedMap = new LinkedHashMap<>();
+        result.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .forEachOrdered(e -> resultSortedMap.put(e.getKey(), e.getValue()));
+        System.out.println(resultSortedMap);    
+    }
+}
---
+++ day17_map_groupby/MapGroupByDemo2.java
@@ -0,0 +1,75 @@
+package com.ocp.day17_map_groupby;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.toSet;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.mapping;
+
+public class MapGroupByDemo2 { //在Map上分組
+    public static void main(String[] args) {
+        List<Fruit> fruits = Arrays.asList(
+                new Fruit("apple", 10, 0.99),
+                new Fruit("banana", 20, 1.99),
+                new Fruit("orange", 10, 2.99),
+                new Fruit("papaya", 10, 3.99),
+                new Fruit("watermelon", 10, 2.99),
+                new Fruit("apple", 20, 0.99),
+                new Fruit("apple", 10, 0.99),
+                new Fruit("papaya", 30, 3.99),
+                new Fruit("apple", 10, 0.99),
+                new Fruit("banana", 10, 1.99)
+        );
+        
+        Map<String, Long> result = fruits.stream().collect(
+            groupingBy(Fruit::getName, counting())
+        );
+        System.out.println(result);
+        
+        Map<String, Integer> sum = fruits.stream().collect(
+            groupingBy(Fruit::getName, summingInt(Fruit::getQty))
+        );
+        System.out.println(sum);
+        
+        // group by price
+        /*
+        {
+            1.99=[Fruit{name=banana, qty=20, price=1.99}, Fruit{name=banana, qty=10, price=1.99}], 
+            0.99=[Fruit{name=apple, qty=10, price=0.99}, Fruit{name=apple, qty=20, price=0.99}, Fruit{name=apple, qty=10, price=0.99}, Fruit{name=apple, qty=10, price=0.99}], 
+            2.99=[Fruit{name=orange, qty=10, price=2.99}, Fruit{name=watermelon, qty=10, price=2.99}], 
+            3.99=[Fruit{name=papaya, qty=10, price=3.99}, Fruit{name=papaya, qty=30, price=3.99}]
+        }
+        */
+        Map<Double, List<Fruit>> groupByPrice = fruits.stream().collect(
+            groupingBy(Fruit::getPrice)
+        );
+        System.out.println(groupByPrice);
+        
+        // group by price
+        /*
+        list
+        {
+            1.99=banana, banana, 
+            0.99=apple, apple, apple, 
+            2.99=orange, watermelon,
+            3.99=papaya, papaya
+        }
+        透過 toSet() 剔除重複資料
+        {
+            1.99=[banana], 
+            0.99=[apple], 
+            2.99=[orange, watermelon], 
+            3.99=[papaya]
+        }
+        */
+        
+        Map<Double, Set<String>> groupByPrice2 = fruits.stream().collect(
+            groupingBy(Fruit::getPrice, mapping(Fruit::getName, toSet()))
+        );
+        System.out.println(groupByPrice2);
+    }
+}
